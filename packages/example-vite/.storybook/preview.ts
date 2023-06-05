36f4cd074e25c11de3a136a594c1cab39d00f305
Torben Larsen <torben.larsen@deptagency.com>
2023-06-05 14:44:59 +0200

    -

--- packages/example-vite/.storybook/preview.ts
+++ packages/example-vite/.storybook/preview.ts
@@ -1,10 +1,6 @@
-import path from 'path';
-
 export const parameters = {
   layout: 'fullscreen',
   liquidjs: {
-    // root: path.resolve(__dirname, '../stories/'),
-    extname: '.liquid' 
   },
 };
 
